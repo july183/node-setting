1d6dddc7fdd2edab129b0d15c4e5d12e89626075
july183 <95838828+july183@users.noreply.github.com>
2022-03-21 15:08:14 +0900

    Delete db directory

--- db/config.js
+++
@@ -1,6 +0,0 @@
-module.exports ={
-    host : "myreactdb-1.chh4wej74n35.ap-northeast-2.rds.amazonaws.com",
-    user : "admin",
-    password : "Bang0820!",
-    database : "introduce_react_interview"
-}
